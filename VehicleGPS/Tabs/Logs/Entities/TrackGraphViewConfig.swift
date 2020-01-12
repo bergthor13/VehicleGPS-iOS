093b7a58ed03c499aeb616046f72ef8d018a6800
Bergþór Þrastarson <bergthor13@ru.is>
2020-01-12 15:14:35 +0000

    Adds TrackGraphConfig class

---
+++ VehicleGPS/Tabs/Logs/Entities/TrackGraphViewConfig.swift
@@ -0,0 +1,19 @@
+//
+//  TrackGraphViewConfig.swift
+//  VehicleGPS
+//
+//  Created by Bergþór Þrastarson on 12/01/2020.
+//  Copyright © 2020 Bergþór Þrastarson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TrackGraphViewConfig {
+    var color = UIColor()
+    var showMinMaxValue = Bool()
+    var numbersList = [Double]()
+    var horizontalLineMarkers = [Double]()
+    var graphMinValue = Double()
+    var graphMaxValue = Double()
+}
