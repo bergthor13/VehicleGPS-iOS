99770bc6fb0b7e965f8c4cc3bae561b3c4e2a6ec
Bergþór Þrastarson <bergthor13@ru.is>
2020-04-01 16:34:33 +0000

    Refactoring AppDelegate

--- VehicleGPS/Shared Entities/AppDelegate.swift
+++ VehicleGPS/Shared Entities/AppDelegate.swift
@@ -13,20 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    var dataStore: VGDataStore?
-    var fileManager: VGFileManager?
-    var tabController: VGTabBarController!
+    var dataStore = VGDataStore()
+    var fileManager = VGFileManager()
+    var tabController = VGTabBarController()
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.tintColor = UIColor.init(named: "appColor")
-
-        self.dataStore = VGDataStore()
-        self.fileManager = VGFileManager()
-        
-        self.tabController = VGTabBarController()
-        
         self.window?.rootViewController = tabController
         self.window?.makeKeyAndVisible()
         return true
@@ -47,31 +41,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
     }
     
-    func application(
-      _ app: UIApplication,
-      open url: URL,
-      options: [UIApplication.OpenURLOptionsKey : Any] = [:]
-    ) -> Bool {
-        window?.rootViewController?.present(UINavigationController(rootViewController: VGImportFileTableViewController(style: .insetGrouped, fileUrl: url)), animated: true)
+    func application(_ app: UIApplication,
+                       open url: URL,
+                       options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        let importController = VGImportFileTableViewController(style: .insetGrouped, fileUrl: url)
+        let navController = UINavigationController(rootViewController: importController)
+        window?.rootViewController?.present(navController, animated: true)
         return true
         
-    }
-}
-
-extension UIColor {
-    convenience init(red: Int, green: Int, blue: Int) {
-        assert(red >= 0 && red <= 255, "Invalid red component")
-        assert(green >= 0 && green <= 255, "Invalid green component")
-        assert(blue >= 0 && blue <= 255, "Invalid blue component")
-        
-        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
-    }
-    
-    convenience init(rgb: Int) {
-        self.init(
-            red: (rgb >> 16) & 0xFF,
-            green: (rgb >> 8) & 0xFF,
-            blue: rgb & 0xFF
-        )
     }
 }
