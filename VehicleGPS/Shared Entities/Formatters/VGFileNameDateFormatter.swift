85f400defa2525fddf0bf37d5081f21c5557411f
Bergþór Þrastarson <bergtor05@me.com>
2020-06-14 00:36:12 +0000

    Adding a filename date formatter

---
+++ VehicleGPS/Shared Entities/Formatters/VGFileNameDateFormatter.swift
@@ -0,0 +1,27 @@
+//
+//  HeaderDateParser.swift
+//  VehicleGPS
+//
+//  Created by Bergþór Þrastarson on 26/01/2020.
+//  Copyright © 2020 Bergþór Þrastarson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class VGFileNameDateFormatter: DateFormatter {
+    override init() {
+        super.init()
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    func configure() {
+        dateFormat = "yyyy-MM-dd hhmmss"
+        locale = Locale(identifier: "en_US_POSIX")
+    }
+}
