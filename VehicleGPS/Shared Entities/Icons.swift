5d8d0432faed3922a7cb443bfc8950f9a2c48bc9
Bergþór Þrastarson <bergtor05@me.com>
2020-07-17 18:47:30 +0000

    Adds more icons

--- VehicleGPS/Shared Entities/Icons.swift
+++ VehicleGPS/Shared Entities/Icons.swift
@@ -14,8 +14,11 @@ struct Icons {
     static let vehicle = UIImage(systemName: "car")
     static let settings = UIImage(systemName: "gear")
     static let share = UIImage(systemName: "square.and.arrow.up")
+    static let moreActions = UIImage(systemName: "ellipsis.circle")
     static let delete = UIImage(systemName: "trash")
     static let add = UIImage(systemName: "plus.circle.fill")
     static let edit = UIImage(systemName: "square.and.pencil")
     static let importFiles = UIImage(systemName: "square.and.arrow.down.on.square.fill")
+    static let photo = UIImage(systemName: "photo")
+    static let split = UIImage(systemName: "square.split.2x1")
 }
